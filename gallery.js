98e14c76f01b957e7ae2482560234e8e83292428
devseyoung <devseyoung@gmail.com>
2020-01-02 11:16:01 +1000

    basic gallery function, but some functions use same code, and they should be fixed by using prototypes

--- gallery.js
+++ gallery.js
@@ -9,15 +9,85 @@ function Gallery(gallery){
   const modal = document.querySelector('.modal');
   const prevButton = modal.querySelector('.prev');
   const nextButton = modal.querySelector('.next');
+  let currentImage;
+
+  function openModal(){
+    console.info('Opening Modal...');
+    // first check if the modal is already open because of animation
+    if(modal.matches('.open')){
+      console.info('Modal already open');
+      return;
+    }
+    modal.classList.add('open');
+    // Event listners to be bound when we open the modal
+    window.addEventListener('keyup', handleKeyUp);
+    nextButton.addEventListener('click', showNextImage);
+    prevButton.addEventListener('click', showPrevImage);
+
+  }
+  function closeModal(){
+    // click outside of modalInnder div and close the modal
+
+    modal.classList.remove('open');
+
+    window.removeEventListener('keyup', handleKeyUp);
+    nextButton.removeEventListener('click', showNextImage);
+    prevButton.removeEventListener('click', showPrevImage);
+
+
+  }
+
+  function handleClickOutside(e){
+    if(e.target === e.currentTarget){
+      closeModal();
+    }
+  }
+
+  function handleKeyUp(event){
+    if(event.key === 'Escape') return closeModal();
+    if(event.key === 'ArrowRight') return showNextImage();
+    if(event.key === 'ArrowLeft') return showPrevImage();
+  }
+
+  function showNextImage(){
+    showImage(currentImage.nextElementSibling || gallery.firstElementChild);
+  }
+
+  function showPrevImage(){
+    showImage(currentImage.previousElementSibling || gallery.lastElementChild);
+  }
 
   function showImage(el){
     if(!el){
       console.info('no image to show');
-      returnl
+      return; // stop the function from running
     }
     // update the modal with this info
     console.log(el);
+    modal.querySelector('img').src = el.src;
+    modal.querySelector('h2').textContent = el.title;
+    modal.querySelector('figure p').textContent = el.dataset.description;
+    currentImage= el;
+    openModal();
   }
+  // function handleImageClick(event){
+  //   showImage(event.currentTarget);
+  // }
+
+  images.forEach(image => image.addEventListener('click',(e) => showImage(e.currentTarget) ));
+
+
+  images.forEach(image => {image.addEventListener('keyup',e => {
+    // when that is keyup'd, check if it was enter
+    if(e.key === 'Enter'){
+      showImage(e.currentTarget);
+    }
+  })
+});
+
+  modal.addEventListener('click',handleClickOutside);
+
+
 }
 
 // use it on the page
